972d93be57c67c55736ca5a9a8d40e45bb4566ed
Vaishnavi sharma <svaishnavi265@gmail.com>
2026-09-01 20:32:08 +0530

    weather api project

---
+++ index.js
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+// const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.api_key}`;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.post('/weather', async (req, res) => {
+    try{
+  const city = req.body.city;
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.api_key}`;  
+  const response = await axios.get(API_URL);
+  const weatherData = response.data;
+  res.render('index.ejs', { weather: weatherData });
+    }catch (error) {
+        console.error('Error fetching weather data:', error);
+        res.status(500).send('Error fetching weather data, check for typos in the city name or try again later.');
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+npm init -y
+npm i
+
+start server at port 3000: nodemon index.js
